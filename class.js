0b4468861d39c01395778faf31dc39bff7990b74
Het <hra.opash@gmail.com>
2026-01-05 15:05:41 +0530

    Class, Inheritance, Abstract

---
+++ class.js
@@ -0,0 +1,15 @@
+// class Person {
+//   user: string | undefined;
+// }
+var Rectangle = /** @class */ (function () {
+    function Rectangle(width, height) {
+        this.width = width;
+        this.height = height;
+    }
+    Rectangle.prototype.getArea = function () {
+        return this.width * this.height;
+    };
+    return Rectangle;
+}());
+var myRect = new Rectangle(10, 20);
+console.log(myRect.getArea());
---
+++ class.ts
@@ -0,0 +1,81 @@
+// class Person {
+//   user: string | undefined;
+// }
+
+// const person = new Person();
+// person.user = "het";
+// console.log(person);
+
+// class Person {
+//   //   private name: string;
+
+//   //   public constructor(name: string) {
+//   //     this.name = name;
+//   //   }
+
+//   public constructor(private name: string) {}
+
+//   public getName(): string {
+//     return this.name;
+//   }
+// }
+
+// const person = new Person("het");
+// console.log(person.getName());
+
+// class Person {
+//   private readonly name: string;
+
+//   public constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   public getName(): string {
+//     return this.name;
+//   }
+// }
+
+// const person = new Person("Het");
+// console.log(person.getName());
+
+// interface Shape {
+//   getArea: () => number;
+// }
+
+// class Rectangle implements Shape {
+//   public constructor(protected readonly width: number, protected readonly height: number) {}
+
+//   public getArea(): number {
+//     return this.width * this.height;
+//   }
+// }
+
+// const myRect = new Rectangle(10,20);
+
+// console.log(myRect.getArea());
+
+
+interface Shape {
+  getArea: () => number;
+}
+
+class Rectangle implements Shape {
+  public constructor(
+    protected readonly width: number,
+    protected readonly height: number
+  ) {}
+
+  public getArea(): number {
+    return this.width * this.height;
+  }
+}
+
+class Square extends Rectangle {
+  public constructor(width: number) {
+    super(width, width);
+  }
+}
+
+const mySq = new Square(20);
+
+console.log(mySq.getArea());
