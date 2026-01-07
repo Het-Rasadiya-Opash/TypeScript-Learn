a181f098e3a479d860f4cea9173563938bc92fb5
Het <hra.opash@gmail.com>
2026-01-07 09:37:27 +0530

    user-interface

---
+++ user-interface.js
@@ -0,0 +1,6 @@
+var user = {
+    username: "User",
+    age: 20,
+    job: "Software Developer",
+};
+console.log(user);
---
+++ user-interface.ts
@@ -0,0 +1,13 @@
+interface User {
+  username: string;
+  age: number;
+  job: string;
+}
+
+const user: User = {
+  username: "User",
+  age: 20,
+  job: "Software Developer",
+};
+
+console.log(user);
